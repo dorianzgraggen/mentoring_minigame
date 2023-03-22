41dcffc5fd3dc6311688d8cf8266ef8fafb6df47
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2023-03-22 16:38:29 +0100

    load gltf

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,8 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(HelloPlugin)
+        .add_startup_system(setup)
         .run();
-}
-
-fn hello_world() {
-    println!("hello world!");
 }
 
 #[derive(Component)]
@@ -23,13 +20,36 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 10.0, 0.0)
+            .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::X),
+        ..default()
+    });
+
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("level.gltf#Scene0"),
+        ..default()
+    });
+}
+
 #[derive(Resource)]
 struct GreetTimer(Timer);
 
 fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
     if timer.0.tick(time.delta()).just_finished() {
         for name in query.iter() {
-            println!("hello {}!", name.0);
+            // println!("hello {}!", name.0);
         }
     }
 }
