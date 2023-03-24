f176152dbb70114ff7e5164b1b27d59f70ae26f0
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2023-03-24 14:09:26 +0100

    try out rapier

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(HelloPlugin)
-        .add_startup_system(setup)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugin(RapierDebugRenderPlugin::default())
+        .add_startup_system(setup_physics)
+        // .add_startup_system(setup)
         .run();
 }
 
@@ -41,6 +45,30 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         scene: asset_server.load("level.gltf#Scene0"),
         ..default()
     });
+}
+
+fn setup_physics(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Add a camera so we can see the debug-render.
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(100.0, 0.1, 100.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(0.5))
+        .insert(Restitution::coefficient(0.7))
+        .insert(SceneBundle {
+            scene: asset_server.load("level.gltf#Scene0"),
+            ..default()
+        })
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)));
 }
 
 #[derive(Resource)]
