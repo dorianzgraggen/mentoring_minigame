3f681909ea4df16316e7a88aaad617c21cde2faf
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2023-03-27 00:23:12 +0200

    tidy up app.js

--- app.js
+++ app.js
@@ -1,36 +1,28 @@
-console.log("Hello", "runjs!");
-
-let content = await fetchText(
-  "https://www.cgbookcase.com/textures/bark-09.json",
-);
-// console.log("fetched", content);
-
-player.setX(-2);
-
-console.log(getStr("nice"));
-
 await onBegin();
 
 let i = 0;
 while (true) {
   try {
     await sleep(16.0);
-
   } catch (error) {
     console.log(error)
   }
 
   globalThis.__runtimeInternal.handleEvents()
-  // await fetchText(
-  //   "https://www.cgbookcase.com/textures/bark-09.json",
-  // );
 
   tick()
   i++;
 }
 
+// ================================================
 
 async function onBegin() {
+
+  let content = await fetchText(
+    "https://www.cgbookcase.com/textures/bark-09.json",
+  );
+  console.log("fetched", content);
+
   controls.addEventListener("keydown", e => {
     console.log(e.data.key_code)
 
@@ -42,10 +34,6 @@ async function onBegin() {
 }
 
 function tick() {
-  // console.log("tick: " + i)
   player.setPosition(Math.sin(i / 12), 0, Math.cos(i / 12));
-
-  // let parsed = globalThis.__runtimeInternal.handleEvents()
-  // console.log("parsed", parsed)
 }
 
